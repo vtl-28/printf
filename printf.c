575f40fb3bed6038020bfdf09d238a25f0c90aeb
wisdomncube <wizzsiyancube@icloud.com>
2023-11-13 09:33:58 -0800

    refactoring code

--- printf.c
+++ printf.c
@@ -37,8 +37,9 @@ int _printf(const char *format, ...)
 	return (-1);
 	while (format[format_iterator] != '\0')
 {
-	struct_iterator = 13;
-	while (struct_iterator > 0)
+	structnd yo_iterator = 13;
+
+	while (struct_iterator >= 0)
 	{
 	if (op[struct_iterator].op[0] == format[format_iterator] &&
 op[struct_iterator].op[1] == format[format_iterator + 1])
