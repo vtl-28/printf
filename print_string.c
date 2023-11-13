7cb0e9e44f40ca25136c24d253c5a300f1402e50
wisdomncube <wizzsiyancube@icloud.com>
2023-11-13 02:30:53 -0800

    refactoring code for task o

--- print_string.c
+++ print_string.c
@@ -1,29 +1,29 @@
 #include "main.h"
 #include <string.h>
 
-
 /**
  * get_string - function to print a string
  * @args: variable arguments
  * Return: return the number of characters printed
  */
+
 int get_string(va_list args)
 {
 	int string_iterator;
 	char *str;
 	int str_length;
-
 	str = va_arg(args, char *);
 
 	if (str == NULL)
-{
+	{
 		str = "(null)";
 			str_length = string_length(str);
 	for (string_iterator = 0; string_iterator < str_length;
 	string_iterator++)
-{
+	{
 	_putchar(str[string_iterator]);
-}
+	}
+
 	return (str_length);
 }
 	else
@@ -31,10 +31,10 @@ int get_string(va_list args)
 	str_length = string_length(str);
 
 	for (string_iterator = 0; string_iterator < str_length;
-		string_iterator++) {
+		string_iterator++)
+	{
 	_putchar(str[string_iterator]);
 }
 	return (str_length);
 }
 }
-
