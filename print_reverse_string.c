a05c9b430e00f2958d0a318d02c58febad68b21f
Vuyisile <vtlehola23@gmail.com>
2023-11-14 10:38:08 -0800

    add fix to printing string in reverse

--- print_reverse_string.c
+++ print_reverse_string.c
@@ -9,20 +9,29 @@
 int get_reverse_string(va_list args)
 {
 	char *str = va_arg(args, char *);
-	int str_iterator, i = 0;
+	int str_i, i = 0;
+	int str_length = 0;
 
 	if (str == NULL)
 	{
 		str = "(null)";
+		str_length = string_length(str);
+
+		for (str_i = 0; str_i < str_length;
+		     str_i++)
+		{
+			_putchar(str[str_i]);
+		}
+		return (str_length);
 	}
 
 	while (str[i] != '\0')
 	{
 		i++;
 
-		for (str_iterator = i - 1; str_iterator >= 0; str_iterator--)
+		for (str_i = i - 1; str_i >= 0; str_i--)
 		{
-			_putchar(str[str_iterator]);
+			_putchar(str[str_i]);
 		}
 	}
 	return (i);
